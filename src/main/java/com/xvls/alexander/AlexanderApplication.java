d1cc84ece0e59f68f743e0278f90f4a7b1f4b35d
GlitterTT <15106696097@163.com>
2020-01-16 21:19:26 +0800

    首次提交，里面有Springboot的WEB、mybatisplus、log4j、mysql、lombok

---
+++ src/main/java/com/xvls/alexander/AlexanderApplication.java
@@ -0,0 +1,13 @@
+package com.xvls.alexander;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlexanderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlexanderApplication.class, args);
+    }
+
+}
